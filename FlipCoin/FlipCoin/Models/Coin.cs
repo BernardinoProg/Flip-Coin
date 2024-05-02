2828004832a6f96e02f0f233bc001574362e73db
Vitor <vitorsouzalibrelonrestini@gmail.com>
2024-05-02 12:40:22 -0300

    jjj

---
+++ FlipCoin/FlipCoin/Models/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin.Models
+{
+    public class Coin
+    {
+        public int Lado { get; set; }
+        public int LadoEscolhido { get; set; }
+
+        public Coin()
+        {
+            
+        }
+
+        public string Jogar()
+        {
+            return new Random().Next(2) == 0 ? "Cara" : "Coroa";
+        }
+    }
+}
